4a395ad2865fde91332dbe1d9ad390ac47d91491
Vanessa Nadal <63133028+vanessanadal@users.noreply.github.com>
2020-04-30 19:52:04 -0400

    FASE1-001: Se añadio el formulario de registro (#1)

--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -1,1 +1,25 @@
-module.exports = {};
+module.exports = {
+  createUser: function (con, body) {
+      return con
+        .query(
+          "INSERT INTO MP_USER (US_IDENTIFICATION,US_FIRST_NAME,US_SECOND_NAME,US_LAST_NAME,US_SECOND_LAST_NAME,US_BIRTHDAY,US_EMAIL,US_PASSWORD,US_PHONE_NUMBER,US_CHARGE,US_FK_LANGUAGE,US_FK_STATUS) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
+          [
+            body.identification,
+            body.first_name,
+            body.second_name,
+            body.last_name,
+            body.second_last_name,
+            body.birthday,
+            body.email,
+            body.password,
+            body.phone_number,
+            body.charge,
+            body.id_language,
+            body.id_status,
+          ]
+        )
+        .catch((error) => {
+          return new Error(error);
+        });
+    }
+};
