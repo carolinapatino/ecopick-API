b21d68eed2fb5aa96c9e81d1d5c471a7e923cbb9
Alba Sofía <assanchez.17@est.ucab.edu.ve>
2020-05-08 10:24:09 -0400

    FASE1-056: Envio de correo de bienvenida con asignación de descuento al usuario

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -1,58 +1,83 @@
 const userModel = require("./user.model");
+const discountModel = require("../discount/discount.model");
 const auth = require("../../middleware/auth");
 const logger = require("../../logger");
+const Email = require("../../utils/Email");
 var createError = require("http-errors");
 
 module.exports = {
-    createUser: async function (req,res,next) {
-        let results = await userModel.createUser(req.con,req.body);
-        if(results instanceof Error) {
-            next(createError(500, `${results.message}`));
-            logger.error({
-                message: `${results.message}`
-            });
-        }
-        else {
-            logger.info({
-                message: `User ${req.body.email} registered successfully`,
-              });
-            res.json({ status: "200"/*, token: auth.createToken()*/});
-        }
-    }, 
-    validateUser: async function (req, res, next) {
-      let results = await userModel.validateUser(req.con,req.body); 
-      if (results instanceof Error) {
+  createUser: async function (req, res, next) {
+    let user = await userModel.createUser(req.con, req.body);
+    if (user instanceof Error) {
+      next(createError(500, `${user.message}`));
+      logger.error({
+        message: `${user.message}`,
+      });
+    } else {
+      let discount = await discountModel.getDiscountByName(req.con, "Welcome");
+      if (discount instanceof Error) {
+        next(createError(500, `${discount.message}`));
         logger.error({
-          message: `${results.message}`,
+          message: `Discount not found | ${discount.message}`,
         });
-        next(createError(500, `${results.message}`));
-      } else { 
-            if (results.length==0) {
-              logger.info({
-                message: `The user entered an invalid email or password `,
-                });
-                //res.json(results);
-                res.json({ status: "401"});
-            } else {
-                if ((results[0].us_charge !== "Client")||(results[0].us_fk_status==5)) { 
-                      if(results[0].us_fk_status==5){
-                          logger.info({
-                          message: `The user ${req.body.email} is disabled.`,
-                          });
-                      } else { 
-                          logger.info({
-                          message: `The user ${req.body.email} hasn't permission to access to the system`,
-                      }); }
-                      //res.json(results);
-                      res.json({ status: "403"});
-                      }  else { 
-                              logger.info({
-                              message: `The user ${req.body.email} has logged in successfully`,
-                              });
-                              //res.json(results);
-                              res.json({ status: "200", token: auth.createToken()});
-                        }
+      } else {
+        logger.info({
+          message: `User ${req.body.email} registered successfully`,
+        });
+        let assignedDiscount = await discountModel.assignDiscount(
+          req.con,
+          user[0].us_id,
+          discount[0].di_id
+        );
+        if (assignedDiscount instanceof Error) {
+          next(createError(500, `${assignedDiscount.message}`));
+          logger.error({
+            message: `Discount not assigned | ${assignedDiscount.message}`,
+          });
+        } else {
+          new Email(req.body.email, req.body.first_name, "Welcome").send(
+            "-" + discount[0].di_percentage * 100
+          );
+        }
+        res.json({ status: "200" /*, token: auth.createToken()*/ });
+      }
+    }
+  },
+  validateUser: async function (req, res, next) {
+    let results = await userModel.validateUser(req.con, req.body);
+    if (results instanceof Error) {
+      logger.error({
+        message: `${results.message}`,
+      });
+      next(createError(500, `${results.message}`));
+    } else {
+      if (results.length == 0) {
+        logger.info({
+          message: `The user entered an invalid email or password `,
+        });
+        //res.json(results);
+        res.json({ status: "401" });
+      } else {
+        if (results[0].us_charge !== "Client" || results[0].us_fk_status == 5) {
+          if (results[0].us_fk_status == 5) {
+            logger.info({
+              message: `The user ${req.body.email} is disabled.`,
+            });
+          } else {
+            logger.info({
+              message: `The user ${req.body.email} hasn't permission to access to the system`,
+            });
+          }
+          //res.json(results);
+          res.json({ status: "403" });
+        } else {
+          logger.info({
+            message: `The user ${req.body.email} has logged in successfully`,
+          });
+          //res.json(results);
+          res.json({ status: "200", token: auth.createToken() });
         }
       }
-    }  
+    }
+  },
 };
--- modules/user/user.model.js
+++ modules/user/user.model.js
@@ -1,32 +1,35 @@
 module.exports = {
   createUser: function (con, body) {
-      return con
-        .query(
-          "INSERT INTO MP_USER (US_IDENTIFICATION,US_FIRST_NAME,US_SECOND_NAME,US_LAST_NAME,US_SECOND_LAST_NAME,US_BIRTHDAY,US_EMAIL,US_PASSWORD,US_PHONE_NUMBER,US_CHARGE,US_FK_LANGUAGE,US_FK_STATUS) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)",
-          [
-            body.identification,
-            body.first_name,
-            body.second_name,
-            body.last_name,
-            body.second_last_name,
-            body.birthday,
-            body.email,
-            body.password,
-            body.phone_number,
-            body.charge,
-            body.id_language,
-            body.id_status,
-          ]
-        )
-        .catch((error) => {
-          return new Error(error);
-        });
-    },
+    return con
+      .query(
+        "INSERT INTO MP_USER (US_IDENTIFICATION,US_FIRST_NAME,US_SECOND_NAME,US_LAST_NAME,US_SECOND_LAST_NAME,US_BIRTHDAY,US_EMAIL,US_PASSWORD,US_PHONE_NUMBER,US_CHARGE,US_FK_LANGUAGE,US_FK_STATUS) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING US_ID",
+        [
+          body.identification,
+          body.first_name,
+          body.second_name,
+          body.last_name,
+          body.second_last_name,
+          body.birthday,
+          body.email,
+          body.password,
+          body.phone_number,
+          body.charge,
+          body.id_language,
+          body.id_status,
+        ]
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
   validateUser: function (con, body) {
-      return con
-        .query("SELECT * FROM MP_USER WHERE us_email=$1 and us_password=$2", [body.email,body.password])
-        .catch((error) => {
-          return new Error(error);
-        });
-    }
+    return con
+      .query("SELECT * FROM MP_USER WHERE us_email=$1 and us_password=$2", [
+        body.email,
+        body.password,
+      ])
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
 };
