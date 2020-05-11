5b906815d98016ec8d7d690a9c31ab8181dd14d5
vanessanadal <vvnadal.17@est.ucab.edu.ve>
2020-05-11 11:35:03 -0400

    FASE1-037: Cambio en metodo de login back office

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -20,39 +20,38 @@ module.exports = {
         }
     }, 
     BO_validateUser: async function (req, res, next) {
-      let results = await userModel.BO_validateUser(req.con,req.body); 
+      let results = await userModel.validateUser(req.con, req.body);
       if (results instanceof Error) {
         logger.error({
-          message: `${results.message}`,
+          message: `STATUS 500 | DATABASE ERROR | ${results.message}`,
         });
         next(createError(500, `${results.message}`));
-      } else { 
-            if (results.length==0) {
-              logger.info({
-                message: `The user entered an invalid email or password `,
-                });
-                //res.json(results);
-                res.json({ status: "401"});
-            } else {
-                if ((results[0].us_charge !== "Admin")||(results[0].us_fk_status==5)) { 
-                      if(results[0].us_fk_status==5){
-                          logger.info({
-                          message: `The user ${req.body.email} is disabled.`,
-                          });
-                      } else { 
-                          logger.info({
-                          message: `The user ${req.body.email} hasn't permission to access to the system`,
-                      }); }
-                      //res.json(results);
-                      res.json({ status: "403"});
-                      }  else { 
-                              logger.info({
-                              message: `The user ${req.body.email} has logged in successfully`,
-                              });
-                              //res.json(results);
-                              res.json({ status: "200", token: auth.createToken()});
-                        }
+      } else if (results.length == 0) {
+        logger.info({
+          message: `STATUS 401 | UNAUTHORIZED | Invalid email or password `,
+        });
+        res.status(401);
+        res.json({});
+      } else if (
+        results[0].us_charge !== "Admin" ||
+        results[0].us_fk_status == 5
+      ) {
+        if (results[0].us_fk_status == 5) {
+          logger.info({
+            message: `STATUS 204 | NO CONTENT | User ${req.body.email} is disabled.`,
+          });
+        } else if (results[0].us_charge !== "Client") {
+          logger.info({
+            message: `STATUS 204 | NO CONTENT | User ${req.body.email} doesn't have permission to access the system`,
+          });
         }
+        res.status(204);
+        res.json({});
+      } else {
+        logger.info({
+          message: `STATUS 200 | OK | User ${req.body.email} has logged in successfully`,
+        });
+        res.json({ token: auth.createToken(), results });
       }
-    }  
+    }, 
 };
