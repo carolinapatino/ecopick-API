63901da8834fd2e6a66aa5a96052e39a823970d3
Alba Sánchez <assanchez.17@est.ucab.edu.ve>
2020-05-14 23:10:35 -0400

    FASE1-056: Notificaciones mediante correo electrónico (#9)
    * FASE1-056: Integración con SendGrid y elaboración de Email.js para enviar cualquier tipo de email
    
    * FASE1-056: Elaboración de correo de bienvenida y correo de descuento
    
    * FASE1-056: Envio de correo de bienvenida con asignación de descuento al usuario
    
    * FASE1-056: Ruta para cambio de contraseña, creación y envio de correo con la nueva contraseña
    
    * FASE1-056: Cambio del método http de actualización de contraseña
    
    * FASE1-056: Elaboración de la ruta para obtener a los usuarios con un rol específico en el sistema
    
    * FASE1-056: Elaboración de la ruta para asignar un nuevo descuento a un usuario y envio de email notificandolo
    
    * FASE1-056: Adición de ruta y plantilla de correo para el envio de un archivo (factura)
    
    * FASE1-056: Modificación del archivo README.md con las dependencias agregadas
    
    * FASE1-056: Arreglando logger y readme

--- modules/user/user.router.js
+++ modules/user/user.router.js
@@ -2,9 +2,32 @@ const express = require("express");
 const router = express.Router();
 const userController = require("./user.controller");
 const auth = require("../../middleware/auth");
+const multer = require("multer");
+var upload = multer({ storage: multer.memoryStorage({}) });
 
+// Registro / Inicio de sesión / Recuperación de contraseña
 router.post("/register", userController.createUser);
 router.post("/login", userController.validateUser);
 router.post("/BO_login", userController.BO_validateUser);
+router.put(
+  "/:id/newPassword",
+  auth.validateToken,
+  userController.forgotPassword
+);
+
+// Manipulación de datos de USER
+router.get("/", auth.validateToken, userController.getUsers);
+router.post(
+  "/:userId/assignDiscount",
+  auth.validateToken,
+  userController.assignDiscount
+);
+
+// Envio de correo con archivo
+router.post(
+  "/sendAttachment",
+  upload.single("file"),
+  userController.sendAttachment
+);
 
 module.exports = router;
