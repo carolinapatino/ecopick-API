ac940cebfcc2cd9b3e0f4448da1ba5f40da32c3e
carolinapatino <cdpatino.17@est.ucab.edu.ve>
2020-05-17 22:24:03 -0400

    FASE1-060: Se agregó la consulta de las caracteristicas

--- modules/characteristic/characteristic.controller.js
+++ modules/characteristic/characteristic.controller.js
@@ -1,4 +1,24 @@
 const characteristicModel = require("./characteristic.model");
 const logger = require("../../config/logger");
 
-module.exports = {};
+module.exports = {
+  getAllCharacteristic: async function (req, res, next) {
+    let results = await characteristicModel.getAllCharacteristic(req.con);
+    if (results instanceof Error) {
+      logger.error({
+        message: `STATUS 500 | DATABASE ERROR | ${results.message}`,
+      });
+      next(createError(500, `${results.message}`));
+    } else if (results.length == 0) {
+      logger.info({
+        message: `STATUS 204 | NO CONTENT | There are not package's characteristics`,
+      });
+      res.status(204);
+    } else {
+      logger.info({
+        message: `STATUS 200 | OK | The package's characteristics was found successfully`,
+      });
+    }
+    res.json(results);
+  },
+};
