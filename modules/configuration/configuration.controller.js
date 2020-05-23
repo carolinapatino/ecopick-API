2b9db7da4b1331499728bcc3295cd9e3c414c003
carolinapatino <cdpatino.17@est.ucab.edu.ve>
2020-05-23 00:43:42 -0400

    FASE1-064: Se agregó la consulta del costo base de envío

--- modules/configuration/configuration.controller.js
+++ modules/configuration/configuration.controller.js
@@ -21,4 +21,23 @@ module.exports = {
     }
     res.json(results);
   },
+  getBaseCost: async function (req, res, next) {
+    let results = await configurationModel.getBaseCost(req.con);
+    if (results instanceof Error) {
+      logger.error({
+        message: `STATUS 500 | DATABASE ERROR | ${results.message}`,
+      });
+      next(createError(500, `${results.message}`));
+    } else if (results.length == 0) {
+      logger.info({
+        message: `STATUS 204 | NO CONTENT | Configuration "Shipping Price" doesn't exist`,
+      });
+      res.status(204);
+    } else {
+      logger.info({
+        message: `STATUS 200 | OK | "Shipping Price" value was found successfuly`,
+      });
+    }
+    res.json(results);
+  },
 };
--- modules/configuration/configuration.model.js
+++ modules/configuration/configuration.model.js
@@ -8,4 +8,13 @@ module.exports = {
         return new Error(error);
       });
   },
+  getBaseCost: function (con) {
+    return con
+      .query(
+        `SELECT CO_VALUE FROM MP_CONFIGURATION WHERE CO_NAME = 'Shipping price'`
+      )
+      .catch((error) => {
+        return new Error(error);
+      });
+  },
 };
