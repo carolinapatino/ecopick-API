0d06c70ab75a498550e96999b17c63eddc4d39a1
carolinapatino <63070963+carolinapatino@users.noreply.github.com>
2020-05-28 20:14:31 -0400

    FASE1-019: Se hace la verificación de la dirección con LOB (#30)
    * FASE1-019: Se hace la verificación de la dirección con LOB
    
    * FASE1-019: Se eliminaron todos los console.log
    
    * FASE1-019: Se elimino el log de error

--- modules/direction/direction.controller.js
+++ modules/direction/direction.controller.js
@@ -1,3 +1,30 @@
 const directionModel = require("./direction.model");
+const logger = require("../../config/logger");
 
-module.exports = {};
+const Lob = require("lob")(process.env.LOB_API_KEY);
+
+module.exports = {
+  test: async function (req, res, next) {
+    let addressVerification = await Lob.usVerifications.verify({
+      primary_line: req.body.primary_line,
+      city: req.body.city,
+      state: req.body.state,
+      zip_code: req.body.zip_code,
+    });
+    if (
+      addressVerification.deliverability == "deliverable" ||
+      addressVerification.deliverability == "deliverable_missing_unit"
+    ) {
+      logger.info({
+        message: `STATUS 200 | THIS DIRECTION IS  DELIVERABLE`,
+      });
+      res.status(200);
+    } else {
+      logger.info({
+        message: `STATUS 204 | THIS DIRECTION IS NOT DELIVERABLE`,
+      });
+      res.status(204);
+    }
+    res.json({});
+  },
+};
--- modules/direction/direction.router.js
+++ modules/direction/direction.router.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 const directionController = require("./direction.controller");
 
+router.post("/verify", directionController.test);
 module.exports = router;
